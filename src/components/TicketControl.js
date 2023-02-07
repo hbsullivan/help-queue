41c8b2dbd8b6f7340156c924859189e4db7bd466
Henry Sullivan <sullivanbhenry@gmail.com>
2023-02-07 10:52:53 -0800

    add more conditionals
    Co-authored-by: Richard Cha <charichard09@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -10,30 +10,59 @@ class TicketControl extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: 0,
+      whatButton: true
     };
   }
 
-  handleClick = () => {
-    this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
-  }
+  // handleClick = () => {
+  //   this.setState(prevState => ({
+  //     formVisibleOnPage: prevState.formVisibleOnPage += 1
+  //   }));
+  // }
 
+  // resetCount = () => {
+  //   this.setState(prevState => ({
+  //     formVisibleOnPage: prevState.formVisibleOnPage = 0
+  //   }));
+  // }
+
+  handleClick = () => {
+    if (this.state.formVisibleOnPage < 3) {
+      this.setState(prevState => ({
+        formVisibleOnPage: prevState.formVisibleOnPage += 1
+      }));
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: prevState.formVisibleOnPage = -1
+      }));
+    }
+  }
+  
   render(){
     let currentlyVisibleState = null;
-    let buttonText = null;
-    if(this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
-      buttonText = "Return to Ticket List";
-    } else {
+    let buttonText = "Add Ticket";
+    if(this.state.formVisibleOnPage === 0) {
       currentlyVisibleState = <TicketList />
       buttonText = "Add Ticket";
+    } else if(this.state.formVisibleOnPage === 1){
+      currentlyVisibleState = <Steps />
+      buttonText = "Yes";
+    } else if(this.state.formVisibleOnPage === 2) {
+      currentlyVisibleState = <Help />
+      buttonText = "Yes";
+    } else if(this.state.formVisibleOnPage === 3) {
+      currentlyVisibleState = <Minutes />
+      buttonText = "Yes";
+    } else{
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return";
     }
     return (
        <React.Fragment>
         {currentlyVisibleState}
         <button onClick={this.handleClick}>{buttonText}</button>
+        {/* <button onClick={this.resetCount}>experiment</button> */}
        </React.Fragment>
     );
   }
